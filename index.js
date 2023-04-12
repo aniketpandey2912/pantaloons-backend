f044a7bef1a7d21d3813ccc2057c89a5b33298e8
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-04-12 19:56:29 +0530

    initial commit, server set

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { connection } = require("./config/db");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("PANTALOONS HOME PAGE");
+});
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB");
+  } catch (err) {
+    console.log("Can't Connect to DB");
+  }
+  console.log("Server running at port", process.env.PORT);
+});
