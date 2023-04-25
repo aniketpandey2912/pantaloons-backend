9f5d5a2d25cae9c4d06006afc6413b9a0f1d7d9b
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-04-25 23:33:37 +0530

    modified login response

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -35,10 +35,18 @@ userRouter.post("/login", async (req, res) => {
   try {
     let users = await UserModel.find({ email });
     if (users.length > 0) {
+      let data = {
+        avatar: users[0].avatar,
+        first_name: users[0].first_name,
+        last_name: users[0].last_name,
+        email: users[0].email,
+        gender: users[0].gender,
+        mobile: users[0].mobile,
+      };
       bcrypt.compare(password, users[0].password, async (err, result) => {
         if (result) {
           let token = jwt.sign({ userID: users[0]._id }, "pantaloons");
-          res.send({ status: true, mssg: "Login successfull", token });
+          res.send({ status: true, mssg: "Login successfull", token, data });
         } else {
           res.send({ status: false, mssg: "Wrong Credentials" });
         }
